cfb767500116111632289f10abab02c5b9b9d5e6
monishski <m.vassantrai@gmail.com>
2021-07-03 11:08:24 +0100

    Completed Twelve Days challenge

--- typescript/twelve-days/twelve-days.ts
+++ typescript/twelve-days/twelve-days.ts
@@ -1,6 +1,36 @@
+const map: { [key: number]: string[] } = {
+  1: ['first', 'a Partridge in a Pear Tree.'],
+  2: ['second', 'two Turtle Doves'],
+  3: ['third', 'three French Hens'],
+  4: ['fourth', 'four Calling Birds'],
+  5: ['fifth', 'five Gold Rings'],
+  6: ['sixth', 'six Geese-a-Laying'],
+  7: ['seventh', 'seven Swans-a-Swimming'],
+  8: ['eighth', 'eight Maids-a-Milking'],
+  9: ['ninth', 'nine Ladies Dancing'],
+  10: ['tenth', 'ten Lords-a-Leaping'],
+  11: ['eleventh', 'eleven Pipers Piping'],
+  12: ['twelfth', 'twelve Drummers Drumming']
+} 
+
 class TwelveDays {
-  static recite(/* Parameters go here */) {
-    // Your code here
+  static recite(start: number, end: number) {
+    let result = ''
+    for (let i = start; i < end + 1; i++) {
+      result += TwelveDays.verse(i)
+    }
+    return result
+  }
+
+  static verse(n: number) {
+    let result = ''
+    let [day, phrase] = map[n]
+    result += `On the ${day} day of Christmas my true love gave to me: ${phrase}`
+    for(let j = n - 1; j >= 1; j--) {
+      let [_, _phrase] = map[j]
+      result += `,${j === 1 ? ' and ' : ' '}${_phrase}`
+    }
+    return result + '\n'
   }
 }
 
